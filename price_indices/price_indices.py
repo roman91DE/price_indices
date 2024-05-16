0bac40fefbec1d10299af49d509e366cc7440054
Roman Hoehn <rohoehn123@gmail.com>
2024-05-16 13:10:37 +0200

    Executed CI Script

--- price_indices/price_indices.py
+++ price_indices/price_indices.py
@@ -43,11 +43,11 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Constants:
-- _PD_DEFAULT_PRICE_COL: Default column name for prices in DataFrame.
+- PD_DEFAULT_PRICE_COL: Default column name for prices in DataFrame.
 - PD_DEFAULT_PRODUCT_ID_COL: Default column name for product IDs in DataFrame.
-- _PD_DEFAULT_QUANTITY_COL: Default column name for quantities in DataFrame.
-- _PD_DEFAULT_TIME_PERIOD_COL: Default column name for time periods in DataFrame.
-- _DEFAULT_NORMALIZATION_VAL: Default normalization value for indices.
+- PD_DEFAULT_QUANTITY_COL: Default column name for quantities in DataFrame.
+- PD_DEFAULT_TIME_PERIOD_COL: Default column name for time periods in DataFrame.
+- DEFAULT_NORMALIZATION_VAL: Default normalization value for indices.
 
 Functions:
 Unweighted Methods (Native Python):
