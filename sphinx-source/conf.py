0fc2a264596b0a61b82391a444b0a34f95771b4b
Roman Hoehn <rohoehn123@gmail.com>
2024-05-16 21:06:45 +0200

    Executed CI Script

---
+++ sphinx-source/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+__module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not __module_path in sys.path:
+    sys.path.insert(0, __module_path)
+
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
+
+project = 'price_indices'
+copyright = '2024, Roman Hoehn'
+author = 'Roman Hoehn'
+release = '0.2'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
